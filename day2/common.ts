cf8538575f75fdc96e40d32ce10b6169f5a4ab13
Matt <matt@kingdomarcade.co.uk>
2025-12-07 21:47:55 +0000

    solve day 2 part 1

---
+++ day2/common.ts
@@ -0,0 +1,40 @@
+export class Range {
+  constructor(public start: number, public end: number) {}
+
+  toPatternedNumbers(): number[] {
+    return Array.from(
+      { length: this.end - this.start + 1 },
+      (_, i) => this.start + i
+    ).filter((num) => {
+      return num.isRepeating();
+    });
+  }
+}
+
+declare global {
+  interface String {
+    toRanges(): Range[];
+  }
+
+  interface Number {
+    isRepeating(): boolean;
+  }
+}
+
+String.prototype.toRanges = function (): Range[] {
+  return this.split(",").map((range) => {
+    const [start, end] = range.split("-").map(Number);
+    return new Range(Number(start), Number(end));
+  });
+};
+
+Number.prototype.isRepeating = function (): boolean {
+  const value = Number(this);
+  if (value.toString().length % 2 !== 0) {
+    return false;
+  }
+
+  const firstHalf = value.toString().slice(0, value.toString().length / 2);
+  const secondHalf = value.toString().slice(value.toString().length / 2);
+  return firstHalf === secondHalf;
+};
